e5244eb70da5ffd4f4f9da1bf598ab0adaa49b53
andris.svarcs <andris.svarcs@avanade.com>
2021-04-07 15:19:37 +0100

    Adding mood board images

--- pages/index.js
+++ pages/index.js
@@ -9,36 +9,48 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>AyaRender</title>
-        <link rel="icon" href="/ayarender-favicon.svg" />
+        <link rel="icon" href="/ayarender-favicon.svg"/>
+        <link rel="mask-icon" href="/ayarender-favicon.svg" color="#000000"/>
+        <link rel="apple-touch-icon" href="/ayarender-apple-touch.png"/>
+        <link rel="manifest" href="/manifest.json"/>
       </Head>
 
-      <section>
-        header part
+      <section className={styles.header}>
+        <Image
+          src="/ayarender-favicon.svg"
+          alt="Ayarender logo"
+          width={100}
+          height={67}
+        />
       </section>
 
       <main className={styles.main}>
         <h1>
-          Welcome to Ayarender!
+          Hi, I create Moodboards!
         </h1>
+
+        <h3>What is a Moodoard?</h3>
+        <p>I create Moodbard to help communicate and present for a client concepts and visual design ideas. </p>
+
         <div>
-        <Image
-          src="/assets/blue.png"
-          alt="Picture of the author"
-          width={300}
-          height={200}
-        />
-        <Image
-          src="/assets/red.png"
-          alt="Picture of the author"
-          width={300}
-          height={200}
-        />
-        <Image
-          src="/assets/green.png"
-          alt="Picture of the author"
-          width={300}
-          height={200}
-        />
+          <Image
+            src="/assets/blue.png"
+            alt="Picture of the author"
+            width={300}
+            height={200}
+          />
+          <Image
+            src="/assets/red.png"
+            alt="Picture of the author"
+            width={300}
+            height={200}
+          />
+          <Image
+            src="/assets/green.png"
+            alt="Picture of the author"
+            width={300}
+            height={200}
+          />
         </div>
       </main>
 
