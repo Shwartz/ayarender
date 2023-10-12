d82a751a53ca924085d3b59a29e5bda3c57d5274
Andris Shvarcs <andris.svarcs@yahoo.com>
2023-10-12 06:44:18 +0100

    Support for SVG, basic colours, fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,41 @@
 <script>
+  import { base } from "$app/paths";
+  import { page } from "$app/stores";
+  import Logo from "/static/icons/logo.svg?component";
   import "./globalStyles.css";
 </script>
 
 <div class="page">
+  <nav>
+    <ul>
+      <li>
+        <a class:active={$page.url.pathname === '/'} href="{base}/">
+          <Logo width="48" height="48" />
+        </a>
+      </li>
+      <li><a class:active={$page.url.pathname === '/'} href="{base}/">Home</a></li>
+      <li><a class:active={$page.url.pathname === '/gallery'} href="{base}/gallery">Gallery</a></li>
+      <li><a class:active={$page.url.pathname === '/contact'} href="{base}/contact">Contact</a></li>
+    </ul>
+  </nav>
   <slot/>
 </div>
+
+<style lang="scss">
+  ul {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 1rem;
+    padding: 4px 0;
+    border-bottom: 1px solid var(--cBlack5);
+
+    a {
+      display: block;
+      &.active,
+      &:focus {
+        text-decoration: underline;
+      }
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero">
+  <h1>Freelance Interior Designer</h1>
+  <h3>Interior Design Visualisation</h3>
+  <nav>
+    <a href="#whatIDo">What I do?</a>
+    <a href="#process">Process</a>
+    <a href="#price">Price</a>
+  </nav>
+</div>
 
+<section id="whatIDo">
+  <div>
+    <h1>What I do?</h1>
+    <p>There is a technical term for what I do - Interior Design visualiser but I would like to call myself Spatial
+      Magician.</p>
+    <p>I can virtually transfer existing living space and show you images of the property that hasn't even been built
+      yet.</p>
+    <p>I specialise in Technical Drawings, Rendered Floor Plans, Rendered 2D Elevations and Design Boards</p>
+  </div>
+</section>
+
+<section>
+  <div>
+    Gallery
+  </div>
+</section>
+
+<section id="process">
+  <div>
+    process
+  </div>
+</section>
+
+<section id="price">
+  <div>
+    price
+  </div>
+</section>
+
+<style lang="scss">
+
+</style>
