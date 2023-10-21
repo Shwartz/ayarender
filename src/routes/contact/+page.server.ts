8f67beee51deb5d59d6bc48e07ab7933189fa9f7
Andris Shvarcs <andris.svarcs@yahoo.com>
2023-10-21 16:29:47 +0100

    Add nodemailer

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -1,22 +1,49 @@
-import { fail } from "@sveltejs/kit";
+import transporter from "$lib/emailSetup.server.js";
 import type { Actions } from './$types';
+import { GOOGLE_EMAIL, RECIPIENT_EMAIL } from "$env/static/private";
 
 export const actions = {
   contactForm: async ({request}) => {
-    const data = await request.formData();
-    const email = data.get('email');
-    const message = data.get('message');
-    console.log({email, message});
+    try {
+      const data = await request.formData();
+      const email = data.get('email');
+      const message = data.get('message');
+      const subject = 'AyaRender contact form'
+      console.log({ email, message });
 
-    if (!email) {
-      return fail(400, { email, missing: true });
+      let html = `<h2>AyaRender Web Form!</h2><p>Email: ${email}</p><pre>${message}</pre>`;
+
+      const mailDetails = {
+        from: GOOGLE_EMAIL,
+        to: RECIPIENT_EMAIL,
+        subject: subject,
+        text: message,
+        html: html,
+      };
+
+      const sendEmail = async (mailDetails) => {
+        await new Promise((resolve, reject) => {
+          transporter.sendMail(mailDetails, (err, info) => {
+            if (err) {
+              console.error(err);
+              reject(err);
+            } else {
+              resolve(info);
+            }
+          });
+        });
+      };
+
+      await sendEmail(mailDetails);
+
+      return {
+        success: true,
+        resEmail: email,
+        resMsg: message
+      };
+    } catch (error) {
+      console.log(error);
+      return {error: error}
     }
-
-    if (!message) {
-      console.log('fail: ', fail(400, { message, missing: true }));
-      return fail(400, { message, missing: true });
-    }
-
-    return {success: true, resEmail: email, resMsg: message};
   }
 } satisfies Actions;
